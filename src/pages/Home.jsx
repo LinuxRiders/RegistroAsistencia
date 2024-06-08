5d90b8a90ff5dba2f0268cb9bbbd200f93f4e4ed
Zartaroth <jsalazarbenavente2014@gmail.com>
2024-06-07 23:57:59 -0500

    Home: Diseño Inicial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,95 @@
-import { Box, Card, Paper } from "@mui/material";
+import { CheckBox } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Card,
+  FormControlLabel,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <Box
       sx={{
+        height: "100vh",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
         textAlign: "center",
-        my: 8,
-        mx: 4,
+        backgroundColor: "#222",
+        p: 0,
+        m: 0,
       }}
     >
-      <Card component={Paper}>CARTA</Card>
+      <Grid
+        container
+        component={Paper}
+        elevation={10}
+        sx={{ width: "50%", p: 3 }}
+      >
+        <Box
+          sx={{
+            my: 8,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate onSubmit="" sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<CheckBox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to="">{"Don't have an account? Sign Up"}</Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Grid>
     </Box>
   );
 }
