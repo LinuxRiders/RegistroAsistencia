2d32b430419f8ee64be8c7299f82c75f89dbe56d
Zartaroth <jsalazarbenavente2014@gmail.com>
2024-06-17 00:24:56 -0500

    Login Dividir en Componentes y Responsive

---
+++ client/src/components/Login/Info.jsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+export default function Info() {
+  return (
+    <Box
+      sx={{
+        p: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+        position: "relative",
+        zIndex: 1,
+      }}
+    >
+      <Typography
+        component="h4"
+        variant="h4"
+        sx={{
+          mb: 2,
+          fontFamily: "Poppins, sans-serif",
+          fontWeight: "bold",
+        }}
+      >
+        V Encuentro de la Red de Calidad de La Universidad Peruana
+      </Typography>
+      <Typography
+        variant="h6"
+        sx={{
+          mb: 2,
+          fontFamily: "Poppins, sans-serif",
+          fontWeight: "bold",
+        }}
+      >
+        Bienvenidos
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{
+          mb: 4,
+          textAlign: "center",
+          fontFamily: "Poppins, sans-serif",
+        }}
+      >
+        Con nuestra experiencia y dedicación, estamos listos para resolver
+        cualquier desafío y crear soluciones innovadoras que te hagan brillar en
+        el mundo virtual. ¡Permítenos ser tus aliados en el viaje hacia el éxito
+        en línea!
+      </Typography>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e4/CRSL_01.png"
+        alt="Logo"
+        style={{ width: 200, height: 200, marginBottom: "16px" }}
+      />
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "rgba(25,77,132,1)",
+          color: "white",
+          mb: 4,
+          fontFamily: "Poppins, sans-serif",
+          fontWeight: "bold",
+        }}
+        href="https://www.uandina.edu.pe/" // Link de ejemplo
+      >
+        UAC
+      </Button>
+      <Typography
+        variant="body1"
+        sx={{ mb: 2, fontFamily: "Poppins, sans-serif" }}
+      >
+        Conócenos en nuestras redes sociales
+      </Typography>
+      <Box sx={{ display: "flex", gap: 2 }}>
+        <Link to="https://www.facebook.com/UniversidadAndinaCusco/?locale=es_LA">
+          <FacebookIcon fontSize="large" sx={{ color: "blue" }} />
+        </Link>
+        <Link to="https://www.instagram.com/universidadandinacusco/">
+          <InstagramIcon fontSize="large" sx={{ color: "purple" }} />
+        </Link>
+      </Box>
+    </Box>
+  );
+}
--- client/src/pages/Login/SignIn.jsx
+++ client/src/pages/Login/SignIn.jsx
@@ -5,9 +5,8 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Link } from "react-router-dom";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import InstagramIcon from "@mui/icons-material/Instagram";
+
+import Info from "../../components/Login/Info";
 
 const defaultTheme = createTheme();
 
@@ -38,22 +37,9 @@ export default function SignIn() {
             backgroundColor: "transparent",
           }}
         >
-          {/* Círculo azul detrás de la columna derecha */}
           <Box
             sx={{
-              position: "absolute",
-              top: "-100%",
-              right: "-55%",
-              width: 1600,
-              height: 1600,
-              borderRadius: "50%",
-              backgroundColor: "rgba(25,77,132,1)",
-              zIndex: 0,
-            }}
-          />
-          <Box
-            sx={{
-              mx: 4,
+              p: 15,
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
@@ -62,72 +48,7 @@ export default function SignIn() {
               zIndex: 1,
             }}
           >
-            <Typography
-              component="h4"
-              variant="h4"
-              sx={{
-                mb: 2,
-                fontFamily: "Poppins, sans-serif",
-                fontWeight: "bold",
-              }}
-            >
-              V Encuentro de la Red de Calidad de La Universidad Peruana
-            </Typography>
-            <Typography
-              variant="h6"
-              sx={{
-                mb: 2,
-                fontFamily: "Poppins, sans-serif",
-                fontWeight: "bold",
-              }}
-            >
-              Bienvenidos
-            </Typography>
-            <Typography
-              variant="body1"
-              sx={{
-                mb: 4,
-                textAlign: "center",
-                fontFamily: "Poppins, sans-serif",
-              }}
-            >
-              Con nuestra experiencia y dedicación, estamos listos para resolver
-              cualquier desafío y crear soluciones innovadoras que te hagan
-              brillar en el mundo virtual. ¡Permítenos ser tus aliados en el
-              viaje hacia el éxito en línea!
-            </Typography>
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/e/e4/CRSL_01.png"
-              alt="Logo"
-              style={{ width: 200, height: 200, marginBottom: "16px" }}
-            />
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: "rgba(25,77,132,1)",
-                color: "white",
-                mb: 4,
-                fontFamily: "Poppins, sans-serif",
-                fontWeight: "bold",
-              }}
-              href="https://www.uandina.edu.pe/" // Link de ejemplo
-            >
-              UAC
-            </Button>
-            <Typography
-              variant="body1"
-              sx={{ mb: 2, fontFamily: "Poppins, sans-serif" }}
-            >
-              Conócenos en nuestras redes sociales
-            </Typography>
-            <Box sx={{ display: "flex", gap: 2 }}>
-              <Link to="https://www.facebook.com/UniversidadAndinaCusco/?locale=es_LA">
-                <FacebookIcon fontSize="large" sx={{ color: "blue" }} />
-              </Link>
-              <Link to="https://www.instagram.com/universidadandinacusco/">
-                <InstagramIcon fontSize="large" sx={{ color: "purple" }} />
-              </Link>
-            </Box>
+            <Info />
           </Box>
         </Grid>
         {/* Sección derecha */}
@@ -144,6 +65,19 @@ export default function SignIn() {
             zIndex: 1,
           }}
         >
+          {/* Círculo azul detrás de la columna derecha */}
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "10%",
+              right: "-100%",
+              width: "200%",
+              height: "200%",
+              borderRadius: "50%",
+              backgroundColor: "rgba(25,77,132,1)",
+              zIndex: 0,
+            }}
+          />
           <Box
             sx={{
               mx: 4,
