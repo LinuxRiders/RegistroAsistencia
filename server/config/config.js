c869818df512d79a1d5c938f5ee686227a8f816e
Zartaroth <jsalazarbenavente2014@gmail.com>
2024-06-19 09:19:34 -0500

    BD Nueva

--- server/config/config.js
+++ server/config/config.js
@@ -4,9 +4,9 @@ config();
 const configs = {
     PORT: process.env.PORT || 4000,
     DB_HOST: process.env.DB_HOST || "videntcusco.com",
-    DB_USER: process.env.DB_USER || "videntcu_olmer",
-    DB_PASSWORD: process.env.DB_PASSWORD || "Jg@MD6EehmHBWwQ",
-    DB_NAME: process.env.DB_NAME || "videntcu_olmer",
+    DB_USER: process.env.DB_USER || "videntcu_asistencia",
+    DB_PASSWORD: process.env.DB_PASSWORD || "2pGRCm59@wQuMEB",
+    DB_NAME: process.env.DB_NAME || "videntcu_asistencia",
     DB_PORT: process.env.DB_PORT || 3306,
 };
 
