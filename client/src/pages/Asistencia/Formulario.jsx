70b709e63627a96cfc39b638662f8fdb9c37c40d
KevynDiego <diegozavaletahurtado@gmail.com>
2024-06-18 20:05:41 -0500

    Diseño Formulario

--- client/src/pages/Asistencia/Formulario.jsx
+++ client/src/pages/Asistencia/Formulario.jsx
@@ -14,7 +14,7 @@ const FormGrid = styled(Grid)(() => ({
 }));
 
 export default function Formulario() {
-  const [DocIdentidad, setDocIdentidad] = useState("");
+  const [nroDoc, setnroDoc] = useState("");
 
   const [nombres, setNombres] = useState("");
   const [ticket, setTicket] = useState("");
@@ -35,7 +35,7 @@ export default function Formulario() {
     setMessageResponse("");
 
     try {
-      const response = await searchUserRequest({ DocIdentidad });
+      const response = await searchUserRequest({ nroDoc });
 
       const json = await response.json();
 
@@ -69,7 +69,7 @@ export default function Formulario() {
     e.preventDefault();
 
     try {
-      const response = await registerUserRequest({ ticket, DocIdentidad });
+      const response = await registerUserRequest({ ticket, nroDoc });
 
       const json = await response.json();
 
@@ -103,22 +103,46 @@ export default function Formulario() {
           width: "100%",
           mb: 3,
           justifyContent: "center",
+          animation: 'fadeIn 2s ease-in-out',
+          '@keyframes fadeIn': {
+            '0%': { opacity: 0 },
+            '100%': { opacity: 1 }
+          }
         }}
       >
-        <img
-          src={"https://www.cipcusco.org.pe/media/dcmpl4rf/logo-web.png"}
+        <img 
+          src="https://i.ibb.co/h7gY1V0/1a.png"
           alt="Consejo Departamental de Cusco"
-          style={{ width: 230, height: "auto", marginRight: 20 }}
-        />
+          style={{
+            width: 210,
+            height: "auto",
+            marginRight: 20,
+            marginBottom: 10,
+            animation: 'zoomIn 2s ease-in-out',
+            '@keyframes zoomIn': {
+              '0%': { transform: 'scale(0.8)', opacity: 0 },
+              '100%': { transform: 'scale(1)', opacity: 1 }
+            }
+          }}>
+        </img>
         <Box sx={{ textAlign: "center" }}>
-          <Typography component="h1" variant="h5" sx={{ fontWeight: "bold" }}>
-            CONSEJO DEPARTAMENTAL DE CUSCO
+          <Typography
+            component="h1"
+            variant="h5"
+            sx={{ fontWeight: "bold", color: "white", animation: 'slideInDown 2s ease-in-out', fontSize: '1.5rem' }}
+          >
+            UNIVERSIDAD ANDINA DEL CUSCO
           </Typography>
-          <Typography component="h2" variant="h6">
-            CONTROL DE REGISTRO DE COLEGIADOS
+          <Typography
+            component="h2"
+            variant="h6"
+            sx={{ color: "white", animation: 'slideInUp 2s ease-in-out', fontSize: '1rem' }}
+          >
+            CONTROL DE REGISTRO DE PARTICIPANTE
           </Typography>
         </Box>
       </Box>
+
       {!!errorResponse && (
         <FormGrid item xs={12}>
           <Alert severity="error" sx={{ width: "90%", my: 2, mx: "auto" }}>
@@ -126,6 +150,7 @@ export default function Formulario() {
           </Alert>
         </FormGrid>
       )}
+
       {!!messageResponse && (
         <FormGrid item xs={12}>
           <Alert severity="success" sx={{ width: "90%", my: 2, mx: "auto" }}>
@@ -133,23 +158,36 @@ export default function Formulario() {
           </Alert>
         </FormGrid>
       )}
+
       {inscrito === "" && consult && (
         <FormGrid item xs={12}>
           <Alert severity="error" sx={{ width: "90%", my: 2, mx: "auto" }}>
-            El Ingeniero no esta Inscrito
+            El participante no está Inscrito
           </Alert>
         </FormGrid>
       )}
+
       <FormGrid item xs={12} md={6}>
         <TextField
           required
           fullWidth
           label="Documento Identidad"
           autoFocus
-          value={DocIdentidad}
-          onChange={(e) => setDocIdentidad(e.target.value)}
+          value={nroDoc}
+          onChange={(e) => setnroDoc(e.target.value)}
+          InputProps={{
+            style: { color: 'white' } // Aquí se establece el color del texto de entrada
+          }}
+          InputLabelProps={{
+            style: { color: 'white' }
+          }}
+          sx={{
+            border: '0.5px solid white', borderRadius: '8px',//Here
+            animation: 'slideInRight 1s forwards',
+          }}
         />
       </FormGrid>
+
       <FormGrid item xs={12} md={6}>
         <TextField
           required
@@ -159,11 +197,17 @@ export default function Formulario() {
           autoFocus
           value={ticket}
           InputLabelProps={{
-            shrink: Boolean(nombres), // Mueve el label hacia arriba cuando hay texto
+            shrink: Boolean(nombres),
+            style: { color: 'white' }
           }}
           onChange={(e) => setTicket(e.target.value)}
+          sx={{
+            border: '0.5px solid white', borderRadius: '8px',//Here
+            animation: 'slideInRight 1s forwards',
+          }}
         />
       </FormGrid>
+
       <FormGrid item xs={12}>
         <TextField
           required
@@ -171,11 +215,22 @@ export default function Formulario() {
           label="Apellidos y Nombre"
           autoFocus
           value={nombres}
+          onChange={(e) => setNombres(e.target.value)}
           InputLabelProps={{
-            shrink: Boolean(nombres), // Mueve el label hacia arriba cuando hay texto
+            shrink: Boolean(nombres),
+            style: { color: 'white' }
+          }}
+          sx={{
+            border: '0.5px solid white', borderRadius: '8px',//Here
+            animation: 'fadeInUp 2s ease-in-out',
+            '@keyframes fadeInUp': {
+              '0%': { opacity: 0, transform: 'translateY(20px)' },
+              '100%': { opacity: 1, transform: 'translateY(0)' }
+            }
           }}
         />
       </FormGrid>
+
       <FormGrid item xs={12} md={6}>
         <TextField
           required
@@ -184,10 +239,20 @@ export default function Formulario() {
           autoFocus
           value={capitulo}
           InputLabelProps={{
-            shrink: Boolean(nombres), // Mueve el label hacia arriba cuando hay texto
+            shrink: Boolean(nombres),
+            style: { color: 'white' }
+          }}
+          sx={{
+            border: '0.5px solid white', borderRadius: '8px',//Here
+            animation: 'slideInRight 2s ease-in-out',
+            '@keyframes slideInRight': {
+              '0%': { transform: 'translateX(-100%)' },
+              '100%': { transform: 'translateX(0)' }
+            }
           }}
         />
       </FormGrid>
+
       <FormGrid item xs={12} md={6}>
         <TextField
           required
@@ -196,10 +261,20 @@ export default function Formulario() {
           autoFocus
           value={asociacion}
           InputLabelProps={{
-            shrink: Boolean(nombres), // Mueve el label hacia arriba cuando hay texto
+            shrink: Boolean(nombres),
+            style: { color: 'white' }
+          }}
+          sx={{
+            border: '0.5px solid white', borderRadius: '8px',//Here
+            animation: 'slideInLeft 2s ease-in-out',
+            '@keyframes slideInLeft': {
+              '0%': { transform: 'translateX(100%)' },
+              '100%': { transform: 'translateX(0)' }
+            }
           }}
         />
       </FormGrid>
+
       <FormGrid item xs={12} md={6}>
         <TextField
           required
@@ -208,10 +283,20 @@ export default function Formulario() {
           autoFocus
           value={inscrito}
           InputLabelProps={{
-            shrink: Boolean(nombres), // Mueve el label hacia arriba cuando hay texto
+            shrink: Boolean(nombres),
+            style: { color: 'white' }
+          }}
+          sx={{
+            border: '0.5px solid white', borderRadius: '8px',//Here
+            animation: 'slideInRight 2s ease-in-out',
+            '@keyframes slideInRight': {
+              '0%': { transform: 'translateX(-100%)' },
+              '100%': { transform: 'translateX(0)' }
+            }
           }}
         />
       </FormGrid>
+
       <FormGrid item xs={12} md={6}>
         <TextField
           required
@@ -220,7 +305,16 @@ export default function Formulario() {
           autoFocus
           value={sede}
           InputLabelProps={{
-            shrink: Boolean(nombres), // Mueve el label hacia arriba cuando hay texto
+            shrink: Boolean(nombres),
+            style: { color: 'white' }
+          }}
+          sx={{
+            border: '0.5px solid white', borderRadius: '8px',//Here
+            animation: 'slideInLeft 2s ease-in-out',
+            '@keyframes slideInLeft': {
+              '0%': { transform: 'translateX(100%)' },
+              '100%': { transform: 'translateX(0)' }
+            }
           }}
         />
       </FormGrid>
@@ -230,19 +324,52 @@ export default function Formulario() {
           type="submit"
           fullWidth
           variant="contained"
-          sx={{ mt: 3, mb: 2, width: "50%", mx: "auto" }}
+          sx={{
+            mt: 3,
+            mb: 2,
+            width: "50%",
+            mx: "auto",
+            backgroundColor: "#183D68",
+            transition: 'background-color 0.3s ease-in-out, transform 0.3s ease-in-out',
+            '&:hover': {
+              backgroundColor: '#2B4E77',
+              transform: 'scale(1.05)'
+            },
+            animation: 'fadeInUp 2s ease-in-out',
+            '@keyframes fadeInUp': {
+              '0%': { opacity: 0, transform: 'translateY(20px)' },
+              '100%': { opacity: 1, transform: 'translateY(0)' }
+            }
+          }}
           onClick={handleConsult}
         >
           Consultar
         </Button>
       </FormGrid>
+
       <FormGrid item xs={6}>
         <Button
-          disabled={inscrito === "" || ticket === 0 ? true : false}
+          disabled={inscrito === "" || ticket === 0}
           type="submit"
           fullWidth
           variant="contained"
-          sx={{ mt: 3, mb: 2, width: "50%", mx: "auto" }}
+          sx={{
+            mt: 3,
+            mb: 2,
+            width: "50%",
+            mx: "auto",
+            backgroundColor: "#183D68",
+            transition: 'background-color 0.3s ease-in-out, transform 0.3s ease-in-out',
+            '&:hover': {
+              backgroundColor: '#2B4E77',
+              transform: 'scale(1.05)'
+            },
+            animation: 'fadeInUp 2s ease-in-out',
+            '@keyframes fadeInUp': {
+              '0%': { opacity: 0, transform: 'translateY(20px)' },
+              '100%': { opacity: 1, transform: 'translateY(0)' }
+            }
+          }}
           onClick={handleSubmit}
         >
           Registrar
